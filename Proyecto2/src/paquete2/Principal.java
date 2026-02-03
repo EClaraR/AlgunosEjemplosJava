a6adeaa6266c0fdf677a5898e29fd03087ccf318
usuario <usuario@DESKTOP-0VSBK2V>
2026-02-03 19:58:14 +0100

    primer commit

---
+++ Proyecto2/src/paquete2/Principal.java
@@ -0,0 +1,11 @@
+package paquete2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejemplo de control de versiones con git");
+
+	}
+
+}
